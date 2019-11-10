e2494b3625f5edd907bea15d11dab6bed705b4be
RandomExplosion <benjaminjmcgaw@gmail.com>
2019-11-10 12:41:07 +1100

    Built Game, New UI (Restart and Quit), Small Changes and Fixes

---
+++ SOTT/Assets/PointAndKill.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndKill : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("PEW");
+
+            //RaycastHit hit;
+            //if(Physics.Raycast(transform.position, transform.forward, 50f, 10, );0
+        }
+    }
+}
---
+++ SOTT/Assets/ToolBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public class ToolBar : MonoBehaviour
+{
+    public TextMeshProUGUI loadingText;
+    public Slider reloadProgress;
+    public GameObject loadingContainer;
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        StartCoroutine("LoadAsyncronouly");
+    }
+
+    IEnumerator LoadAsyncronouly()
+    {
+
+        AsyncOperation loading = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex); //Begin Loading
+        loadingContainer.SetActive(true); //Show the Loading bar and label
+
+        while (!loading.isDone)
+        {
+            Debug.Log("Reload progress: " + loading.progress); //Update in console
+            reloadProgress.value = loading.progress*100; //Update the slider
+            loadingText.SetText("Loading: <color=#00AFFF>" + Mathf.Round(loading.progress*100) + "% </color>"); //Update Label
+            yield return null;
+        }
+    }
+}

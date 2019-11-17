2ae285f18e5be776fccfd20a25d2704c320bcfa6
RandomExplosion <benjaminjmcgaw@gmail.com>
2019-11-17 17:23:45 +1100

    fixed wandering problems implimenting breakpoint toggling at runtime

---
+++ SOTT/Assets/BeanDebugger2020.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeanDebugger2020 : MonoBehaviour
+{
+    public Toggle toggle;
+    public static bool _beanDemandsDebug = false;
+    
+    public void Change(bool state)
+    {
+        _beanDemandsDebug = state;
+    }
+}
